8ead0b8f0591843517bebb373768ae7cabacd659
amelialim888 <amelialimlh@gmail.com>
2025-07-02 20:54:41 +0800

    Testing submission

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -13,7 +13,7 @@ function App() {
   return (
     <>
       <div>
-        <img src={financeLogo} className="logo react" alt="Finance logo" />
+        <img src={financeLogo} className="logo react" alt="Finance logo" /> 
       </div>
       <h1>Finance Dashboard</h1>
       <StockForm />
